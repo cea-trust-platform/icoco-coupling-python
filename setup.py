fbcaebc9517468ea3ffdbde96ca18956d7266397
roland-lenain <124668641+roland-lenain@users.noreply.github.com>
2023-03-08 15:20:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -137,7 +137,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
-    install_requires=["numpy==1.19.5"],  # Optional
+    install_requires=["numpy>=1.19.5"],  # Optional
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
